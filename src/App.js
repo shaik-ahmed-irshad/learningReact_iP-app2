8485ea69ad726c06c6765433f961be581e91657a
shaik-ahmed-irshad <edventurous.ahmed@gmail.com>
2024-03-24 16:17:55 +0530

    displaying dynamic from axios in react table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Loading from "./Loading";
 function App() {
+  const [totalData, setTotaldata] = useState([]);
+  const [data, setData] = useState([]);
+  const [completed, setCompleted] = useState(true);
+  const [loading,setLoading]=useState(false)
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true)
+        let res = await axios.get(`https://jsonplaceholder.typicode.com/todos`);
+        // artificial delay for loading animation. #badPractice
+        // setTimeout(()=>{
+            setLoading(false);
+            setData(res.data);
+            setTotaldata(res.data);
+        // },500)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, []);
+
+  function filterTodos() {
+    if (completed) {
+      const filteredData = totalData.filter((ele) => ele.completed);
+      setData(filteredData);
+    } else {
+      const filteredData = totalData.filter((ele) => !ele.completed);
+      setData(filteredData);
+    }
+    setCompleted(!completed);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading && <Loading />}
+      <center>
+        <div className="table-responsive">
+          <button onClick={filterTodos}>Task Filter</button>
+          <table className="table table-primary">
+            <thead>
+              <tr>
+                <th className="th">Id</th>
+                <th className="th">User Id</th>
+                <th className="th">Title</th>
+                <th className="th">Completed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((ele) => (
+                <tr key={ele.id}>
+                  <td>{ele.id}</td>
+                  <td>{ele.userId}</td>
+                  <td>{ele.title}</td>
+                  <td>{ele.completed ? "True" : "False"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </center>
+    </>
   );
 }
 
